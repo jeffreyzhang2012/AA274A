e39e219e78a1771ae7c95f3d5938a25f54149868
jeffreyzhang2012 <jefzhangxy@hotmail.com>
2021-10-13 13:26:24 -0700

    s3 done

--- s3/code/odom_subscriber.py
+++ s3/code/odom_subscriber.py
@@ -1,12 +1,14 @@
 import rospy
-
+from nav_msgs.msg import Odometry
 
 def callback(data):
-    pass
+    rospy.loginfo(data)
 
 
 def subscriber():
-    pass
+    rospy.init_node('listenner', anonymous=True)
+    rospy.Subscriber("odom", Odometry, callback)
+    rospy.spin()
 
 
 if __name__ == '__main__':
--- s3/code/vel_publisher.py
+++ s3/code/vel_publisher.py
@@ -1,8 +1,15 @@
 import rospy
-
+from geometry_msgs.msg import Twist
 
 def publisher():
-    pass
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+    rospy.init_node('talker',anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        move_cmd = Twist()
+        move_cmd.linear.x = 0.0
+        pub.publish(move_cmd)
+        rate.sleep()
 
 
 if __name__ == '__main__':
